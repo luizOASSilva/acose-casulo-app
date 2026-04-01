68145c4651c7ed7e835d643cc7f7263e0d891f1a
Luiz Silva <160427605+luizOASSilva@users.noreply.github.com>
2026-03-31 22:18:00 -0300

    feature: criando migrations para as tabelas administrador e documento

---
+++ database/migrations/2026_04_01_011233_create_documento_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documento', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('administrador_Id')->constrained();
+            $table->string('arquivo_url')->unique();
+            $table->dateTime('created_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documento');
+    }
+};
