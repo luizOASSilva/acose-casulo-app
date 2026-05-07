d004fd105d40bc3ed56a6458de07470f23996faa
Luiz Silva <160427605+luizOASSilva@users.noreply.github.com>
2026-05-07 15:33:25 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -53,3 +53,10 @@ Route::middleware('auth:sanctum')->group(function () use ($resources) {
 
     Route::apiResource('admins', AdminController::class);
 });
+
+Route::get('/limpar-tudo', function () {
+    \Illuminate\Support\Facades\Artisan::call('config:clear');
+    \Illuminate\Support\Facades\Artisan::call('cache:clear');
+    \Illuminate\Support\Facades\Artisan::call('route:clear');
+    return response()->json(['message' => 'Cache limpo no Railway!']);
+});
