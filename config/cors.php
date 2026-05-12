02a07b619dae68a2dd06ac4ae95cae99b08a9ea7
Luiz Silva <160427605+luizOASSilva@users.noreply.github.com>
2026-05-12 18:08:59 -0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,23 @@
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'https://acose-casulo-web.vercel.app',
+        'http://localhost:3000',
     ],
 
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
 
     'supports_credentials' => true,
 ];
