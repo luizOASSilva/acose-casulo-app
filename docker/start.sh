28a0e2cc957a007d69ad3ad0f0fff6a644e1cbc5
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2026-05-14 23:49:11 -0300

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-chmod -R 775 storage bootstrap/cache
-chown -R www-data:www-data storage bootstrap/cache
-
-# ← adiciona isso
 mkdir -p /var/www/html/storage/logs
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/cache
+mkdir -p /var/www/html/storage/framework/views
+
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
+touch /var/www/html/storage/logs/laravel.log
 ln -sf /dev/stdout /var/www/html/storage/logs/laravel.log
 
 php artisan config:clear
