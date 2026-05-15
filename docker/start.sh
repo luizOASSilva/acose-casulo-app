b60e185333ae83aafc36b3bb675328a2851bf788
Luiz Silva <160427605+luizOASSilva@users.noreply.github.com>
2026-05-14 22:00:03 -0300

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -3,15 +3,16 @@
 chmod -R 775 storage bootstrap/cache
 chown -R www-data:www-data storage bootstrap/cache
 
+# ← adiciona isso
+mkdir -p /var/www/html/storage/logs
+ln -sf /dev/stdout /var/www/html/storage/logs/laravel.log
+
 php artisan config:clear
 php artisan cache:clear
 php artisan view:clear
 php artisan route:clear
 php artisan migrate --force
 php artisan config:cache
-
-# ← adiciona isso para ver logs do Laravel no Render
-ln -sf /dev/stdout /var/www/html/storage/logs/laravel.log
 
 php-fpm -D
 nginx -g "daemon off;"
