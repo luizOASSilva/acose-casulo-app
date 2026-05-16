ca20be8cdbf9e293128bd9ab76ff3f14e8b2d67c
Luiz Silva <160427605+luizOASSilva@users.noreply.github.com>
2026-05-15 21:05:20 -0300

    Update AdminResource.php

--- app/Http/Resources/Admin/AdminResource.php
+++ app/Http/Resources/Admin/AdminResource.php
@@ -1,17 +1,12 @@
 <?php
 
-namespace App\Http\Resources;
+namespace App\Http\Resources\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AdminResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
